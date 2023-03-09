15f50b6cb3740ee244fa805a60e47a4635598d5d
Kiran-7700 <121047254+Kiran-7700@users.noreply.github.com>
2023-03-09 22:25:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -20,14 +20,14 @@ function removeListFom(){
 
 
 
-document.querySelector("#addList").addEventListener("click",createListForm);
+document.querySelector("#addList").addEventListener("click",createListForm); //list add
 
 function createListForm(){
    let userInput=document.querySelector("#listName").value;
    document.querySelector("#listForm").style.display="none";
-   let div1=document.createElement("div");
+   let div1=document.createElement("div");  // card create
    div1.classList.add("newCard");
-   let title=document.createElement("h4");
+   let title=document.createElement("h4");   
    title.classList.add("listHeading");
      div1.appendChild(title);
    title.innerText=userInput;
@@ -35,11 +35,11 @@ function createListForm(){
      div1.appendChild(hr);
     let img=document.createElement("img");
     img.classList.add("trashImage")
-    img.setAttribute("src","trash.jpg");
+    img.setAttribute("src","trash.jpg");  //add trash img
     div1.appendChild(img,);
     let img2=document.createElement("img");
     img2.classList.add("cardPlusIcon");
-    img2.setAttribute("src","cardplus.jpg");
+    img2.setAttribute("src","cardplus.jpg");   //add plus img
     div1.appendChild(img2);
     document.querySelector("#container").style.opacity = "1";
     document.querySelector("#secondContainer").style.opacity="1"
@@ -57,35 +57,36 @@ function createListForm(){
     }
 
 
-       let newCards=document.getElementsByClassName("newCard")
 
 
 
 
-     let addItemBtn=document.querySelector("#addItem");
-     addItemBtn.addEventListener("click",addItems)
-     function addItems (){
+       let addItemBtn=document.querySelector("#addItem");   //add item button
+       addItemBtn.addEventListener("click",addItems)  
+       function addItems (){
+
        let userInpItem=document.querySelector("#itemList").value;
        document.querySelector("#itemForm").style.display="none";
        document.querySelector("#container").style.opacity = "1";
        div1.style.opacity="1"
        let p=document.createElement("p");
         p.setAttribute("class","pid");
+        
       
 
       
-        p.innerText=userInpItem;
-        let span=document.createElement("span");
-        span.innerHTML=`<button class="markDoneBtn" id="markDoneBtn">Mark Done</button>`;
+        p.innerText = userInpItem;
+        let span = document.createElement("span");
+        span.innerHTML = `<button class="markDoneBtn" id="markDoneBtn">Mark Done</button>`; //mark done button
+        span.firstChild.addEventListener("click", function () {
+          lineThrough(p);
+          this.style.display = "none";
+        });
         p.appendChild(span);
         div1.appendChild(p);
-        document.querySelector(".markDoneBtn").addEventListener("click", function(){
-          lineThrough(p);
-          document.querySelector(".markDoneBtn").style.display="none"
-        });
        
       
-        //  addItemBtn.removeEventListener("click",addItems);
+          addItemBtn.removeEventListener("click",addItems);
      }
       
      
@@ -104,19 +105,22 @@ function createListForm(){
       div1.style.opacity="1"
      }
 
-   document.querySelector("#secondContainer").appendChild(div1);
+     document.querySelector("#secondContainer").appendChild(div1);
 
 
 
-   title.addEventListener("click",openCard);
-   function openCard(){
+     title.addEventListener("click",openCard);   //one card open
+     function openCard(){
      document.querySelector("#head").style.display="none";
-    document.querySelector("#backBtn").style.display="block";
+     document.querySelector("#container").style.display="none"
+     document.querySelector("#backBtn").style.display="block";
+     let getText=document.querySelector("#getText");
+     getText.innerHTML=userInput;
 
-   }
+    }
 }
 
-    let backIcon=document.querySelector("#goBackIcon");
+    let backIcon=document.querySelector("#goBackIcon");   //go back button
     backIcon.addEventListener("click",backToMainPage);
     function backToMainPage(){
          
