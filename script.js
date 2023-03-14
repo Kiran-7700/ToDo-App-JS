408b1e7b85d283e1ce3aebfa7e2fc7434711f9af
Kiran-7700 <121047254+Kiran-7700@users.noreply.github.com>
2023-03-14 18:12:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 document.getElementById("icon").addEventListener("click",crateList);
+let cards=[];
 function crateList(){
   let form = document.querySelector("#listForm")
-    form.style.display="block"
+    form.style.display="block";
     document.querySelector("#container").style.opacity = "0.1";
-    document.querySelector("#secondContainer").style.opacity="0.1"
+    document.querySelector("#secondContainer").style.opacity="0.1";
 }
 
 
-
-
 document.querySelector("#close").addEventListener("click",removeListFom);
-
 function removeListFom(){
   document.querySelector("#listForm").style.display="none"
   document.querySelector("#container").style.opacity = "1";
@@ -18,10 +16,7 @@ function removeListFom(){
 }
 
 
-
-
 document.querySelector("#addList").addEventListener("click",createListForm); //list add
-
 function createListForm(){
    let userInput=document.querySelector("#listName").value;
    document.querySelector("#listForm").style.display="none";
@@ -42,7 +37,7 @@ function createListForm(){
     img2.setAttribute("src","cardplus.jpg");   //add plus img
     div1.appendChild(img2);
     document.querySelector("#container").style.opacity = "1";
-    document.querySelector("#secondContainer").style.opacity="1"
+    document.querySelector("#secondContainer").style.opacity="1";
     img.addEventListener("click",removeCard);
     function removeCard(){
       div1.style.display="none";
@@ -54,49 +49,39 @@ function createListForm(){
       let itemForm=document.querySelector("#itemForm");
       div1.style.opacity="0.1";
       itemForm.style.display="block";
-    }
 
 
+      let addItemBtn=document.querySelector("#addItem");   //add item button
+      addItemBtn.addEventListener("click",addItems)  
+      function addItems (){
 
-
-
-
-       let addItemBtn=document.querySelector("#addItem");   //add item button
-       addItemBtn.addEventListener("click",addItems)  
-       function addItems (){
-
-       let userInpItem=document.querySelector("#itemList").value;
-       document.querySelector("#itemForm").style.display="none";
-       document.querySelector("#container").style.opacity = "1";
-       div1.style.opacity="1"
-       let p=document.createElement("p");
-        p.setAttribute("class","pid");
-        
-      
-
-      
-        p.innerText = userInpItem;
-        let span = document.createElement("span");
-        span.innerHTML = `<button class="markDoneBtn" id="markDoneBtn">Mark Done</button>`; //mark done button
-        span.firstChild.addEventListener("click", function () {
-          lineThrough(p);
-          this.style.display = "none";
-        });
-        p.appendChild(span);
-        div1.appendChild(p);
-       
-      
-          addItemBtn.removeEventListener("click",addItems);
-     }
+      let userInpItem=document.querySelector("#itemList").value;
+      document.querySelector("#itemForm").style.display="none";
+      document.querySelector("#container").style.opacity = "1";
+      div1.style.opacity="1";
+      let p=document.createElement("p");
+       p.setAttribute("class","pid");
+       p.innerText = userInpItem;
+       let span = document.createElement("span");
+       span.innerHTML = `<button class="markDoneBtn" id="markDoneBtn">Mark Done</button>`; //mark done button
+       span.firstChild.addEventListener("click", function () {
+         lineThrough(p);
+         this.style.display = "none";
+       });
+       p.appendChild(span);
+       div1.appendChild(p);
       
      
+       addItemBtn.removeEventListener("click",addItems);
+      }
+    }
      function lineThrough(element) {
       // let p=document.querySelector(".pid");
       element.style.textDecoration = "line-through";
     }
 
 
-
+     //item selected box deleted
      let deleteBox=document.querySelector("#closeItem");
      deleteBox.addEventListener("click",deleteItemBox);
      function  deleteItemBox(){
@@ -105,26 +90,45 @@ function createListForm(){
       div1.style.opacity="1"
      }
 
-     document.querySelector("#secondContainer").appendChild(div1);
-
-
-
+     cards.push(div1);
+     console.log(cards);  
+     display(cards)
+     
+        function display(cards){
+          let container=document.querySelector("#secondContainer");
+          for (let i=0; i<cards.length;i++){
+            container.appendChild(cards[i]);
+          }
+        }
+        
      title.addEventListener("click",openCard);   //one card open
      function openCard(){
-     document.querySelector("#head").style.display="none";
-     document.querySelector("#container").style.display="none"
-     document.querySelector("#backBtn").style.display="block";
+       document.querySelector("#head").style.display="none";
+     document.querySelector("#text").style.display="none";
+      document.querySelector("#secondContainer").style.display=" none";
+      document.querySelector("#backBtn").style.display="block";
      let getText=document.querySelector("#getText");
      getText.innerHTML=userInput;
-
+     div1.style.display="block";
+     let newDivForCard=document.getElementById("newDivForCard");
+     newDivForCard.appendChild(div1,getText)
     }
-}
+    
 
     let backIcon=document.querySelector("#goBackIcon");   //go back button
     backIcon.addEventListener("click",backToMainPage);
     function backToMainPage(){
-         
+      let ele=document.querySelector("#backBtn");
+      let ele2=document.querySelector("#newDivForCard");
+      ele.style.display="none";
+      ele2.style.display="none";
+    document.querySelector("#secondContainer").style.display="flex";
+    document.querySelector("#head").style.display="block";
+    document.querySelector("#text").style.display="block";
    }
+}
+
+
 
 
 
